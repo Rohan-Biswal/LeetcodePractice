8f107446b8ff8b7c9398b354125a9c59ec8a58c5
Rohan-Biswal <rohanbiswal03@gmail.com>
2024-06-03 19:36:30 +0530

    two sum problem

--- twoSumProblem.java
+++ twoSumProblem.java
@@ -2,17 +2,24 @@ package LeetcodePractice;
 
 public class twoSumProblem {
     public static int[] twoSumProb(int array[],int target){
+        if(array.length==0){
+            System.out.println("Array is empty");
+            System.exit(-145);
+        }
+        else{
             for(int i=0;i<array.length-1;i++){
                 for(int j=1;j<array.length;j++){
                     if(array[i]+array[j]==target){
                         return new int[] {i,j};
                     }
+
                 }
             }
-            return new int[]{};
+        }
+        return new int[]{};
     }
     public static void main(String[] args) {
-        int [] array ={1,8,13,24,5};
+        int [] array ={};
         int target=14;
         int [] result=twoSumProb(array,target);
         System.out.println("The target is found at index "+result[0]+" and "+result[1]);
