52f5930378307c91a59caa4cecde34ae7c830ec0
Rohan-Biswal <rohanbiswal03@gmail.com>
2024-06-29 22:32:13 +0530

    Plus One Problem

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,31 @@
+package LeetcodePractice.Arrays;
+import java.util.*;
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits=new int[digits.length+1];
+        digits[0]=1;
+        return digits;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of Array");
+        int length=sc.nextInt();
+        int[] array=new int[length];
+        System.out.println("Enter the element in array");
+        for(int i=0;i<length;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("After adding one in last index the array is-");
+        int[] result=plusOne(array);
+        for(int i=0;i<length;i++){
+            System.out.print(result[i]+" ");
+        }
+    }
+}
