b6038961d00345cd84e5fc4be527caf287ec7cd8
Rohan-Biswal <rohanbiswal03@gmail.com>
2024-07-02 22:53:25 +0530

    Contain Duplicate In Array Problem

---
+++ Arrays/ContainDuplicate.java
@@ -0,0 +1,26 @@
+package LeetcodePractice.Arrays;
+import java.util.*;
+public class ContainDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of Array");
+        int length=sc.nextInt();
+        int[] array=new int[length];
+        System.out.println("Enter the value in Array");
+        for(int i=0;i<length;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("The array has duplicate value-"+containsDuplicate(array));
+    }
+    
+}
+
