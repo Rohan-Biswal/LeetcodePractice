f6c54cbe83e60e7bc72cc555393be7f94bc7eb69
Rohan-Biswal <rohanbiswal03@gmail.com>
2024-09-03 23:06:58 +0530

    Height Checker Problem

---
+++ Arrays/HeightChecker.java
@@ -0,0 +1,30 @@
+package LeetcodePractice.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HeightChecker {
+
+    public static int heightChecker(int[] heights) {
+        int[] sortedHeights = heights.clone();
+        Arrays.sort(sortedHeights);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of Array");
+        int length=sc.nextInt();
+        int[] array=new int[length];
+        System.out.println("Enter the element in array");
+        for(int i=0;i<length;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("The number of indices where heights[i] != expected[i] is-"+heightChecker(array));
+    }
+}
